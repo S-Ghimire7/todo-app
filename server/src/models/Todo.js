ae0bb02ec4d63949532a1d9505bf92ec2977defc
unknown <foot123trick@gmail.com>
2026-07-30 09:26:28 +0545

    Initial commit: todo app ready for deployment

---
+++ server/src/models/Todo.js
@@ -0,0 +1,29 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../configs/database.js";
+
+export const Todo = sequelize.define(
+  "TodoItem",
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    deadline: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    isUrgent: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "todos",
+    timestamps: true,
+  },
+);
